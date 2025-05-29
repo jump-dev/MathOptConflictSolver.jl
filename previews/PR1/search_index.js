b44fbd2031352ad12b5b2c12f6fd3abbdf4f2001
Documenter.jl <documenter@juliadocs.github.io>
2025-05-29 07:12:39 +0000

    build based on ff3a571

---
+++ previews/PR1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MathOptConflictSolver.jl","page":"MathOptConflictSolver.jl","title":"MathOptConflictSolver.jl","text":"","category":"section"},{"location":"","page":"MathOptConflictSolver.jl","title":"MathOptConflictSolver.jl","text":"This package is a basic conflict solver for MathOptInterface.jl.","category":"page"}]
+}
